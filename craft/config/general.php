ca7491785ef8d946445c044ed5993c3d43276426
Ryan Belisle <reeboot@reeboot.net>
2014-03-08 01:24:28 -0500

    Revert bdfad5b..9ab8e8b
    This rolls back to commit bdfad5bc1350720f595f1a21e57bae78baeb9b93.

--- craft/config/general.php
+++ craft/config/general.php
@@ -20,7 +20,7 @@ return array(
         'devMode' => true,
         'environmentVariables' => array(
             'siteUrl'        => 'http://ryanbelisle.dev/',
-            'fileSystemPath' => '../ryanbelisle/images/',
+            'fileSystemPath' => '../public/images/',
         ),
     ),
 
